02997f33c718a6275803324fc59b91a3a5a23815
lostdragon <dragon829@gmail.com>
2020-04-24 10:33:44 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,19 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+#修改版本号
+sed -i 's/OpenWrt/Lostdragon AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
+#移除不用软件包
+rm -rf package/lean/luci-app-dockerman
+#添加额外软件包
+git clone https://github.com/bin20088/luci-app-koolddns.git package/luci-app-koolddns
+git clone https://github.com/bin20088/luci-theme-butongwifi.git package/luci-theme-butongwifi
+git clone https://github.com/Repobor/luci-app-koolproxyR.git package/luci-app-koolproxyR
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+#赋予koolddns权限
+chmod 0755 package/luci-app-koolddns/root/etc/init.d/koolddns
+chmod 0755 package/luci-app-koolddns/root/usr/share/koolddns/aliddns
+./scripts/feeds update -a
+./scripts/feeds install -a
